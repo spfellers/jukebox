975095c226ac4c1bcca70f9e47fa84abfeb0762a
spfellers <sfellers@purdue.edu>
2018-05-26 16:35:06 -0400

    added login route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'jukebox';
+  author = 'spfellers';
+  email = 'spfellers@gmail.com';
 }
